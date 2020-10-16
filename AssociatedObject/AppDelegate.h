e131c6bbaffa09d5adaa436950ff8eb60a780768
Company_DuBenben <2249923489@qq.com>
2020-10-16 16:50:43 +0800

    Initial Commit

---
+++ AssociatedObject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AssociatedObject
+//
+//  Created by DuBenben on 2020/10/16.
+//  Copyright © 2020 CNKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AssociatedObject/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  AssociatedObject
+//
+//  Created by DuBenben on 2020/10/16.
+//  Copyright © 2020 CNKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
